de78c6d2c49e659107849ee384df26198b700cbf
Rupesh Chaudhari <rcashok0@gmail.com>
2019-01-11 00:08:32 +0530

    a

--- music/views.py
+++ music/views.py
@@ -5,7 +5,7 @@ from django.shortcuts import render, get_object_or_404
 from .models import Album, song
 
 
-def index(request):
+def Index(request):
     albums = Album.objects.all()
     context = {
         'albums': albums
@@ -22,3 +22,31 @@ def album_details(request, album_id):
     except Album.DoesNotExist:
         return render(request, '404.html', {'album_id': album_id})
     return render(request, 'details.html', context)
+
+
+def fav(request, album_id):
+    album = Album.objects.get(id=album_id)
+    return render(request, 'details.html')
+
+
+def search(request):
+    sparam = request.POST['search']
+    noalbum = sparam
+    s_status = 'success'
+    albums = Album.objects.filter(album_title__contains=sparam)
+    if(not (albums)):
+        s_status = 'danger'
+        noalbum = 'No albums Found '
+    return render(request, 'index.html', {'noalbum': noalbum, 's_status': s_status, 'albums': albums})
+
+
+def create_album(request):
+    return render(request, 'index.html')
+
+
+def update_album(request):
+    return render(request, 'index.html')
+
+
+def delete_album(request):
+    return render(request, 'index.html')
