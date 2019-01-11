0928d30634817dbf1cace85ba6877f2c35701150
Rupesh Chaudhari <rcashok0@gmail.com>
2019-01-12 01:25:00 +0530

    ..

--- music/views.py
+++ music/views.py
@@ -24,15 +24,20 @@ def album_details(request, album_id):
     return render(request, 'details.html', context)
 
 
-def fav(request, song_id):
+def fav(request, album_id):
+    album = Album.objects.get(id=album_id)
+    song_id = request.POST['song']
     songsel = get_object_or_404(song, id=song_id)
-    if(songsel.is_fav):
+    if(songsel.is_fav == True):
+        error_message = "Song UnFavourited"
+        error_type = "danger"
         songsel.is_fav = False
     else:
         songsel.is_fav = True
-    error_message = "Song Favourited"
-    error_type = "success"
-    return render(request, 'details.html', {'error_message': error_message, 'error_type': error_type})
+        error_message = "Song Favourited"
+        error_type = "success"
+    songsel.save()
+    return render(request, 'details.html', {'album': album, 'error_message': error_message, 'error_type': error_type})
 
 
 def search(request):
@@ -50,14 +55,14 @@ def search(request):
 
 def create_album(request):
     mesg_status = "warning"
-    if(request.POST['artist'] and request.POST['title'] and request.POST['genre'] and request.FILES['album_logo']):
+    if(request.POST and request.POST['artist'] and request.POST['title'] and request.POST['genre'] and request.FILES['album_logo']):
         album = Album()
         album.artist = request.POST['artist']
         album.album_title = request.POST['title']
         album.genre = request.POST['genre']
         album.album_logo = request.FILES['album_logo']
         album.save()
-        mesg = " Album created successfully"
+        mesg = "Album created successfully"
         mesg_status = "success"
         return render(request, 'details.html', {'album': album, 'mesg': mesg, 'mesg_status': mesg_status})
     else:
@@ -66,8 +71,21 @@ def create_album(request):
     return render(request, 'create_album.html', {'mesg': mesg, 'mesg_status': mesg_status})
 
 
-def update_album(request):
-    return render(request, 'index.html')
+def update_album(request, album_id):
+    album = Album.objects.get(id=album_id)
+    mesg_status = "warning"
+    mesg = "hello"
+    if(request.POST and request.POST['artist'] and request.POST['title'] and request.POST['genre'] and request.FILES['album_logo']):
+        album = Album()
+        album.artist = request.POST['artist']
+        album.album_title = request.POST['title']
+        album.genre = request.POST['genre']
+        album.album_logo = request.FILES['album_logo']
+        album.save()
+        mesg = " Album updated successfully"
+        mesg_status = "success"
+        return render(request, 'details.html', {'album': album, 'mesg': mesg, 'mesg_status': mesg_status})
+    return render(request, 'update_album.html', {'mesg': mesg, 'mesg_status': mesg_status, 'album': album})
 
 
 def delete_album(request):
